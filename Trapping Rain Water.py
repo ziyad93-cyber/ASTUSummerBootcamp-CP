5dce4246420dfa04b994fc2f7d2ce3b1fe4fc174
Ziyad ayub dewano <ziyadayub93@gmail.com>
2026-06-26 23:58:31 +0300

    Implement Trapping Rain Water algorithm

---
+++ Trapping Rain Water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        if not height:
+            return 0
+        
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        water_trapped = 0
+        
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water_trapped += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water_trapped += right_max - height[right]
+                
+        return water_trapped
