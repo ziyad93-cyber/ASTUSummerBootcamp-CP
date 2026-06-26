949d69cf1d98f922eafc09499341eeed0114a3e9
Ziyad ayub dewano <ziyadayub93@gmail.com>
2026-06-27 00:01:50 +0300

    Implement function to find maximum average subarray

---
+++ Maximum Average Subarray I.py
@@ -0,0 +1,10 @@
+def findMaxAverage(nums: list[int], k: int) -> float:
+    current_sum = sum(nums[:k])
+    max_sum = current_sum
+    
+    for i in range(k, len(nums)):
+        current_sum = current_sum + nums[i] - nums[i - k]
+        max_sum = max(max_sum, current_sum)
+        
+    
+    return max_sum / k
